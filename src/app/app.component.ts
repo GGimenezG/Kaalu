47cc1de4c3aa0e2ac7950f2aabb26f8e2e41e44d
Guillermo Giménez <gg091195@gmail.com>
2018-04-20 13:18:37 -0400

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { InicioPage } from '../pages/inicio/inicio';
+import {RegistroPage} from '../pages/registro/registro';
+import { LoginPage } from '../pages/login/login';
+import { PrincipalPage } from '../pages/principal/principal';
+import { SeguirPage } from '../pages/seguir/seguir';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { CategoriaPage } from '../pages/categoria/categoria';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+export class MyApp {
+
+  @ViewChild('NAV') nav: Nav;
+  public rootPage= InicioPage;
+  private pages: Array<{titulo: string, component: any, icon: string}>;
+
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen) {
+    
+    this.pages = [
+      {titulo: 'Inicio', component: PrincipalPage, icon: 'home'},
+      {titulo: 'Contactos', component: SeguirPage, icon: 'contacts'},
+      {titulo: 'Categorías', component: CategoriaPage, icon: 'apps'},
+      {titulo: 'Mensajes', component: CategoriaPage, icon: 'chatbubbles'},
+      {titulo: 'Configuración', component: CategoriaPage, icon: 'settings'},
+      {titulo: 'Cerrar sesión', component: LoginPage, icon: 'log-out'},
+
+    ];
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +42,16 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  goToPerfil(){
+    this.nav.setRoot(PerfilPage);
+  }
+  
+  goToPage(page){
+    this.nav.setRoot(page);
+  }
+
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+  
+import { MyApp } from './app.component'; 
+import { LoginPage } from '../pages/login/login';
+import { SeguirPage } from '../pages/seguir/seguir';
+import { PrincipalPage } from '../pages/principal/principal';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { CategoriaPage } from '../pages/categoria/categoria';
+import { InicioPage } from '../pages/inicio/inicio';
+import { RegistroPage } from '../pages/registro/registro';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
-@NgModule({
+
+@NgModule({  
   declarations: [
     MyApp,
-    HomePage
-  ],
+    LoginPage,
+    SeguirPage,
+    PrincipalPage,
+    PerfilPage,
+    CategoriaPage,
+    InicioPage,
+    RegistroPage,
+    ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
@@ -19,7 +33,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    SeguirPage,
+    PrincipalPage,
+    PerfilPage,
+    CategoriaPage,
+    RegistroPage,
+    InicioPage,
   ],
   providers: [
     StatusBar,
@@ -28,3 +48,4 @@ import { HomePage } from '../pages/home/home';
   ]
 })
 export class AppModule {}
+ 
